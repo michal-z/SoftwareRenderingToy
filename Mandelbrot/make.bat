275c6e672fa09086b102ab33175baba5b695ff42
michal-z <michalziulek@gmail.com>
2019-03-20 20:48:21 +0100

    Added top level make.bat

--- Mandelbrot/make.bat
+++ Mandelbrot/make.bat
@@ -2,15 +2,15 @@
 setlocal
 setlocal enableextensions
 
-::set NAME=Mandelbrot
-set NAME=MandelbrotAVX2
+set NAME=Mandelbrot
 
 if "%1" == "clean" if exist Common.pch del Common.pch
 
 set CFLAGS=/Zi /I..\Common /nologo /W4 /EHa- /arch:AVX2 /wd4204 /wd4201
+set DEBUG=/Od /D_DEBUG
+set RELEASE=/O2 /fp:fast /DNDEBUG
 
-set CONFIG=/O2 /fp:fast /DNDEBUG
-::set CONFIG=/Od /D_DEBUG
+if not defined CONFIG set CONFIG=%DEBUG%
 
 if exist %NAME%.exe del %NAME%.exe
 
@@ -18,6 +18,10 @@ if not exist Common.pch (cl %CONFIG% %CFLAGS% /c /YcCommon.h ..\Common\Common.c)
 if ERRORLEVEL 1 goto :end
 
 cl %CONFIG% %CFLAGS% /YuCommon.h %NAME%.c /link Common.obj kernel32.lib user32.lib gdi32.lib /incremental:no /opt:ref
+if ERRORLEVEL 1 goto :end
+
+cl %CONFIG% %CFLAGS% /YuCommon.h %NAME%AVX2.c /link Common.obj kernel32.lib user32.lib gdi32.lib /incremental:no /opt:ref
+if ERRORLEVEL 1 goto :end
 
 :end
 if exist %NAME%.obj del %NAME%.obj
